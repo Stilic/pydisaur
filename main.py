77df6ab3d096897e45e8cc159705530d6f056579
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2021-04-14 08:22:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def shorten_success():
 def urls():
     r = Query()
     user = discord.fetch_user()
-    return render_template("urls.html", l=db.search(r.creator.id == user.id))
+    return render_template("urls.html", l=db.search(r.creator_id == user.id))
 
 
 @app.route("/api/deleteShorten/")
